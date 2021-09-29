2d33da40509dd9e238d66e101362a7c492396bb9
davidtaylor <sfcdavidtaylor@gmail.com>
2021-09-28 20:09:07 -0600

    Added JSON

--- main.py
+++ main.py
@@ -3,37 +3,62 @@ from datetime import datetime
 from os.path import dirname
 import os.path
 import csv
-import psutil
 import time
 import pip
+import subprocess
+import sys
+import json
+import psutil
 
+
+#installs psutil
+#def install_and_import(package):
+#    import importlib
+#    try:
+#        importlib.import_module(package)
+#    except ImportError:
+#        import pip
+#        pip.main(['install', package])
+#    finally:
+#        globals()[package] = importlib.import_module(package)
+#install_and_import('psutil')
 
 #variables
-max_pct = 902
+
 user_time = 1
 # my attempt at a universal path
 expand = (dirname(__file__))
 folder = os.path.join(expand, 'monitor.csv')
+
 #gets user input
-user_time = input('\n\nHow many minutes would you like to monitor?\nPlease input minutes and press enter: ')
-print('You selected: ' + user_time + ' minutes')
+#user_time = input('\n\nHow many minutes would you like to monitor?\nPlease input minutes and press enter: ')
+
+
+config_file = open('config.json',)
+data = json.load(config_file)
+print('View settings:\n')
+print(data)
+utime = data['utime']
+delay_interval = data['delay_interval']
+t_end = data['t_end']
+max_pct = data['max_pct']
 #changes user input from string to float
-user_float = float(user_time)
+#user_float = float(user_time)
+
 #sets the sketch to run for time 60 sec x 15; we can replace with chron
-t_end = time.time() + 60 * user_float
+t_end = time.time() + 60 * utime
 #gets delay inerval
-delay_interval = float(input('Input how fast you want the CPU/Memory to be polled and press enter. \n Seconds: '))
+
+#delay_interval = float(input('Input how fast you want the CPU/Memory to be polled and press enter. \n Seconds: '))
 print('Results will be recorded to file located in ' + expand)
 print('\n\nYYYY_MM_DD_HH:MM:SS CPU% RAM%')
+
+
+
+
 #main program
 while time.time() < t_end:
     def main():
-        # Make sure we've got psutil
-        #try:
-        #    __import__(psutil)
-        #except ImportError:
-        #    pip.main(['install', psutil])
-
         # date time stamp
         dt = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
         # load average for 1 minute
